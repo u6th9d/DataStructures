83f907f7309a9b523e486eece0d1a8240af877fd
u6th9d <u6th9d@outlook.com>
2015-06-05 14:19:01 +0800

    add SeqQueuePro

---
+++ SeqQueuePro/SeqQueue.c
@@ -0,0 +1,93 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"SeqQueue.h"
+
+typedef struct _tag_SeqQueue {
+	int capacity, length;
+	int rear, front;
+	void** node;
+}TSeqQueue;
+
+SeqQueue* SeqQueue_Create(int capacity) {
+	TSeqQueue* ret = NULL;
+	if (capacity >= 0) {
+		ret = (TSeqQueue*)malloc(sizeof(TSeqQueue) + capacity * sizeof(void*));
+		if (ret != NULL) {
+			ret->capacity = capacity;
+			ret->length = 0;
+			ret->rear = 0;
+			ret->front = 0;
+			ret->node = (void**)(ret + 1);
+		}
+	}
+	return ret;
+}
+
+int SeqQueue_Destroy(SeqQueue* queue) {
+	int ret = (queue != NULL);
+	if (ret) {
+		free(queue);
+	}
+	return ret;
+}
+
+int SeqQueue_Clear(SeqQueue* queue) {
+	TSeqQueue* tQueue = (TSeqQueue*)queue;
+	int ret = (tQueue != NULL);
+	if (ret) {
+		tQueue->length = 0;
+		tQueue->front = tQueue->rear;
+	}
+	return ret;
+}
+
+int SeqQueue_Append(SeqQueue* queue, void* item) {
+	TSeqQueue* tQueue = (TSeqQueue*)queue;
+	int ret = (tQueue != NULL);
+	ret = (tQueue->capacity > tQueue->length);
+	if (ret) {
+		tQueue->node[tQueue->rear] = item;
+		tQueue->rear = (tQueue->rear + 1) % tQueue->capacity;
+		tQueue->length++;
+	}
+	return ret;
+}
+
+void* SeqQueue_Retrieve(SeqQueue* queue) {
+	TSeqQueue* tQueue = (TSeqQueue*)queue;
+	void* ret = NULL;
+	int i = 0;
+	if (tQueue != NULL) {
+		ret = tQueue->node[tQueue->front];
+		tQueue->front = (tQueue->front + 1) % tQueue->capacity;
+		tQueue->length--;
+	}
+	return ret;
+}
+
+void* SeqQueue_Header(SeqQueue* queue) {
+	TSeqQueue* tQueue = (TSeqQueue*)queue;
+	void* ret = NULL;
+	if (tQueue != NULL) {
+		ret = tQueue->node[tQueue->front];
+	}
+	return ret;
+}
+
+int SeqQueue_Length(SeqQueue* queue) {
+	TSeqQueue* tQueue = (TSeqQueue*)queue;
+	int ret = -1;
+	if (tQueue != NULL) {
+		ret = tQueue->length;
+	}
+	return ret;
+}
+
+int SeqQueue_Capacity(SeqQueue* queue) {
+	TSeqQueue* tQueue = (TSeqQueue*)queue;
+	int ret = -1;
+	if (tQueue != NULL) {
+		ret = tQueue->capacity;
+	}
+	return ret;
+}
